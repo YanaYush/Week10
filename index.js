355720dfea6ffe25bd343c425bccb4aaf9fb1134
Yana <yanadurst@gmail.com>
2023-02-19 21:49:04 +0300

    html and js files were added, assets folder with images and css file inside

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function showMessage() {
+
+    console.log('Я изучаю JavaScript!')
+}
+
+showMessage();
+
+
+let img1 = document.getElementById('img1')
+
+
+function showPicture() {
+
+    img1.src = './assets/images/la-vp2_big.jpg'
+
+}
+
+function show2Picture() {
+
+    img1.src = './assets/images/los-angeles.jpg'
+
+}
+
+
+
